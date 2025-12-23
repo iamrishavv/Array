89a47e5b54d651300e7e2edf6f5708bf6bce2a34
Rishav <95118742+iamrishavv@users.noreply.github.com>
2025-12-23 17:00:40 +0530

    Add files via upload

---
+++ twoDimArray.java
@@ -0,0 +1,12 @@
+package Array.assigment;
+//2d array
+public class twoDimArray {
+    public static void main(String[] args) {
+        int [][]arr={{1,2,3},{4,5,6}};
+        for(int[] x:arr){
+            for(int j:x){
+                System.out.println(j);
+            }
+        }
+    }
+}
